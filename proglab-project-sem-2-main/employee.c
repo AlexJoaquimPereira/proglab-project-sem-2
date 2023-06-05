687710532b5809017a965bfec7c36f5579f1afd8
Alex <alexpereira1781@gmail.com>
2023-06-05 17:48:54 +0530

    Formatted employee.c, added sample files This is my final submission, the project's work is complete  Changes to be committed: 	modified:   proglab-project-sem-2-main/employee.c 	new file:   proglab-project-sem-2-main/john.txt 	new file:   proglab-project-sem-2-main/melissa.txt 	new file:   proglab-project-sem-2-main/stacey.txt

--- proglab-project-sem-2-main/employee.c
+++ proglab-project-sem-2-main/employee.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
 
 typedef struct
 {
@@ -19,36 +17,30 @@ int main()
 	int eage, p, s;
 
 	employee *e;
-	printf("\nEnter the number of employee records:\n");
+	printf("Enter the number of employee records:\n");
 	scanf("%d", &n);
 
 	e = (employee *)malloc(n*sizeof(employee));
 
-	printf("\nEnter the employee details:\n");
+	printf("Enter the employee details:\n");
 	for (i = 0; i < n; i++)
 	{
-		printf("\nEnter the filename to create: ");
+		printf("Enter the filename to create: ");
 		scanf("%s", filename);
-
-		/*printf("\nEnter employee name :");
-		scanf("%s", e[i].ename);
-
-		strcpy(filename, e[i].ename);
-		strcat(filename, ".txt");*/
 
 		fp = fopen(filename, "w");
 		printf("File created: %s\n", filename);
-		printf("\nEnter employee name :");
+		printf("Enter employee name :");
 		scanf("%s", e[i].ename);
-		printf("\nEnter employee age :");
+		printf("Enter employee age :");
 		scanf("%d", &e[i].eage);
-		printf("\nEnter employee designation: ");
+		printf("Enter employee designation: ");
 		scanf("%s", e[i].designation);
 
 		fprintf(fp, "%s %d %s", e[i].ename, e[i].eage, e[i].designation);
 		
-		printf("\nContents written to file successfully.....\n");
-		printf("\n-----------------------------------\n");
+		printf("Contents written to file successfully.....\n");
+		printf("-----------------------------------\n");
 
 		fclose(fp);
 	}
@@ -71,63 +63,63 @@ int main()
 				fp = fopen(filename, "r");
 				if (fp == NULL)
 				{
-					printf("\nSorry filename does not exits...");
+					printf("Sorry filename does not exits...\n");
 					goto p;
 				}
 				
-				printf("\nEmployee details is as follows:\n");
+				printf("Employee details is as follows:\n");
 				if (q == 0)
 				{
 					fscanf(fp, "%s %d %s", ename, &eage, designation);
-					printf("\nName:=%s  Age:=%d  Designation:=%s", ename, eage, designation);
+					printf("Name:=%s  Age:=%d  Designation:=%s\n", ename, eage, designation);
 				}
 				else
 				{
 					fscanf(fp, "%s %d %s %s", ename, &eage, designation, address);
-					printf("\nName:=%s  Age:=%d  Designation:=%s  Address=%s", ename, eage, designation, address);
+					printf("Name:=%s  Age:=%d  Designation:=%s  Address:=%s\n", ename, eage, designation, address);
 				}
 				fclose(fp);
-				printf("\n------------------------------------");
+				printf("------------------------------------\n");
 				break;
 			}
 			case 2:
 			{
-				printf("\n\nEnter filename to update age: ");
+				printf("Enter filename to update age: ");
 				scanf("%s", filename);
 
 				fp = fopen(filename, "r");
 				fscanf(fp, "%s %d", ename, &eage);
-				printf("\nEnter new age: ");
+				printf("Enter new age: ");
 				scanf("%d", &nage);
 				eage = nage;
 				fclose(fp);
 
 				fp = fopen(filename, "w");
 				fprintf(fp, "%s %d", ename, eage);
-				printf("\nContent updated to file successfully.....");
+				printf("Content updated to file successfully.....\n");
 				fclose(fp);
-				printf("\n------------------------------------");
+				printf("------------------------------------\n");
 				break;
 			}
 			case 3:
 			{
-				printf("\n\nEnter filename to append address: ");
+				printf("Enter filename to append address: ");
 				scanf("%s", filename);
 
 				fp = fopen(filename, "a");
-				printf("\nEnter address to append to the file: ");
+				printf("Enter address to append to the file: ");
 				scanf("%s", address);
 				fprintf(fp, " %s", address);
-				printf("\nAddress appended to file successfully.........");
+				printf("Address appended to file successfully.........\n");
 				q++;
 				fclose(fp);
 				break;
 			}
 		}
-	p: fflush(stdin); // clears the standard i/o buffer
+	p: fflush(stdin);
 
-		printf("\n----------------------------");
-		printf("\n\nEnter y to continue or n to quit\n");
+		printf("----------------------------\n");
+		printf("Enter y to continue or n to quit\n");
 		scanf("%c", &t);
 	} while (t != 'n');
 
